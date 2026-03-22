c3c5c74d763942b6653781fe4e5fa035d3b3ba6f
aza1rat <artemkashityn193@gmail.com>
2026-03-22 17:25:12 +0300

    Уточнение возвращаемых типов VacancyDao

--- app/src/main/java/ru/practicum/android/diploma/db/data/dao/VacancyDao.kt
+++ app/src/main/java/ru/practicum/android/diploma/db/data/dao/VacancyDao.kt
@@ -3,6 +3,6 @@ package ru.practicum.android.diploma.db.data.dao
 interface VacancyDao {
     suspend fun insert(vacancy: Any)
     suspend fun remove(id: String)
-    suspend fun getAllByPage(offset: Int)
-    suspend fun getById(id: String)
+    suspend fun getAllByPage(offset: Int): List<Any>
+    suspend fun getById(id: String): Any
 }
