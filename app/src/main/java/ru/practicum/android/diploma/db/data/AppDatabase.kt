abf7811947488687230fb36c31f3eb820f3904ad
aza1rat <artemkashityn193@gmail.com>
2026-03-22 17:20:41 +0300

    Добавлены слои db, di, team

--- app/src/main/java/ru/practicum/android/diploma/db/data/AppDatabase.kt
+++ app/src/main/java/ru/practicum/android/diploma/db/data/AppDatabase.kt
@@ -3,6 +3,6 @@ package ru.practicum.android.diploma.db.data
 import androidx.room.RoomDatabase
 import ru.practicum.android.diploma.db.data.dao.VacancyDao
 
-abstract class AppDatabase: RoomDatabase() {
+abstract class AppDatabase : RoomDatabase() {
     abstract fun getVacancyDao(): VacancyDao
 }
